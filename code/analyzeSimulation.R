626c32d17a0c1951715a1d6f4278dd3b15c61e48
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2021-05-14 20:16:44 -0700

    enhanced simulation

--- code/analyzeSimulation.R
+++ code/analyzeSimulation.R
@@ -4,10 +4,31 @@ library(readr)
 library(ggplot2)
 
 df <- read_table2("output/simulation.txt", col_names = FALSE)
-df <- df[,1:3]
-colnames(df) <- c("Model","Decimals","Covered")
+df <- df[,1:5]
+colnames(df) <- c("Model","Decimals","Covered95", "Covered80","Covered50")
 df$Model <- factor(df$Model)
 df$Decimals <- factor(df$Decimals)
-df$Covered <- as.numeric(df$Covered)
+df$Covered95 <- as.numeric(df$Covered95)
+df$Covered80 <- as.numeric(df$Covered80)
+df$Covered50 <- as.numeric(df$Covered50)
 
-tab <- table(df)[,,2] / (table(df)[,,2] + table(df)[,,1])
+library(reshape2)
+df <- melt(df,measure.vars=3:5,value.name = "Covered",variable.name = "Level")
+df1 <- df[df$Level=="Covered95",]
+df1 <- df1[,-3]
+
+tab1 <- table(df1)[,,2] / (table(df1)[,,2] + table(df1)[,,1])
+tab1
+
+df1 <- df[df$Level=="Covered80",]
+df1 <- df1[,-3]
+
+tab2 <- table(df1)[,,2] / (table(df1)[,,2] + table(df1)[,,1])
+tab2
+
+df1 <- df[df$Level=="Covered50",]
+df1 <- df1[,-3]
+
+tab3 <- table(df1)[,,2] / (table(df1)[,,2] + table(df1)[,,1])
+tab3
+
--- code/simulation.R
+++ code/simulation.R
@@ -1,7 +1,7 @@
 setwd("~/unknown_locs")
 source("code/simulateHawkes.R")
 library(hpHawkes)
-set.seed(777)
+set.seed(666)
 
 for(i in 1:1000) {
   digits <- 1
@@ -11,7 +11,7 @@ for(i in 1:1000) {
   times <- output[,3] 
   
   
-  out <- hpHawkes::simulation_sampler(n_iter=15000,
+  out <- hpHawkes::simulation_sampler(n_iter=30000,
                                       locations=X,
                                       times=times,
                                       gpu=2,
@@ -23,13 +23,26 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
                             0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("sampledLocations", digits, spat_length_covered, "\n",
+  cat("sampledLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
-  out <- hpHawkes::simulation_sampler(n_iter=10000,
+  locsArray <- simplify2array(out$locations)
+  locsQuant <- apply(locsArray,MARGIN = c(1,2), FUN=function(x) quantile(x,probs=c(0.025,0.975))) 
+  locsCovered <- output[,1:2] < locsQuant[2,,] & output[,1:2] > locsQuant[1,,]
+  propCovered <- sum(locsCovered)/length(locsCovered)
+  
+  cat(digits, propCovered, "\n",
+      file = "output/simulationLocsCoverage.txt", append = TRUE, sep=" ")
+  
+  out <- hpHawkes::simulation_sampler(n_iter=15000,
                                        locations=X,
                                        times=times,
                                        gpu=2,
@@ -40,17 +53,22 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
-                            0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+                               0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("fixedLocations", digits, spat_length_covered, "\n",
+  cat("fixedLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
   ##############################################################################
   X <- round(2*output[,1:2])/2
   digits <- 0.5
   
-  out <- hpHawkes::simulation_sampler(n_iter=15000,
+  out <- hpHawkes::simulation_sampler(n_iter=30000,
                                       locations=X,
                                       times=times,
                                       gpu=2,
@@ -62,13 +80,26 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
-                            0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+                               0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("sampledLocations", digits, spat_length_covered, "\n",
+  cat("sampledLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
-  out <- hpHawkes::simulation_sampler(n_iter=10000,
+  locsArray <- simplify2array(out$locations)
+  locsQuant <- apply(locsArray,MARGIN = c(1,2), FUN=function(x) quantile(x,probs=c(0.0125,0.9875))) #bonferroni correction
+  locsCovered <- output[,1:2] < locsQuant[2,,] & output[,1:2] > locsQuant[1,,]
+  propCovered <- sum(locsCovered)/length(locsCovered)
+  
+  cat(digits, propCovered, "\n",
+      file = "output/simulationLocsCoverage.txt", append = TRUE, sep=" ")
+  
+  out <- hpHawkes::simulation_sampler(n_iter=15000,
                                       locations=X,
                                       times=times,
                                       gpu=2,
@@ -79,10 +110,15 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
-                            0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+                               0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("fixedLocations", digits, spat_length_covered, "\n",
+  cat("fixedLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
   
@@ -91,7 +127,7 @@ for(i in 1:1000) {
   X <- round(output[,1:2])
 
   
-  out <- hpHawkes::simulation_sampler(n_iter=15000,
+  out <- hpHawkes::simulation_sampler(n_iter=30000,
                                       locations=X,
                                       times=times,
                                       gpu=2,
@@ -103,13 +139,26 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
-                            0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+                               0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("sampledLocations", digits, spat_length_covered, "\n",
+  cat("sampledLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
-  out <- hpHawkes::simulation_sampler(n_iter=10000,
+  locsArray <- simplify2array(out$locations)
+  locsQuant <- apply(locsArray,MARGIN = c(1,2), FUN=function(x) quantile(x,probs=c(0.0125,0.9875))) #bonferroni correction
+  locsCovered <- output[,1:2] < locsQuant[2,,] & output[,1:2] > locsQuant[1,,]
+  propCovered <- sum(locsCovered)/length(locsCovered)
+  
+  cat(digits, propCovered, "\n",
+      file = "output/simulationLocsCoverage.txt", append = TRUE, sep=" ")
+  
+  out <- hpHawkes::simulation_sampler(n_iter=15000,
                                       locations=X,
                                       times=times,
                                       gpu=2,
@@ -120,10 +169,15 @@ for(i in 1:1000) {
   
   samps <- out$samples
   se_spat_length <- 1/samps[1,]
-  spat_length_covered <- (0.5 < quantile(se_spat_length,probs = 0.975) &
-                            0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_95 <- (0.5 < quantile(se_spat_length,probs = 0.975) &
+                               0.5 > quantile(se_spat_length,probs = 0.025) )
+  spat_length_covered_80 <- (0.5 < quantile(se_spat_length,probs = 0.9) &
+                               0.5 > quantile(se_spat_length,probs = 0.1) )
+  spat_length_covered_50 <- (0.5 < quantile(se_spat_length,probs = 0.75) &
+                               0.5 > quantile(se_spat_length,probs = 0.25) )
   
-  cat("fixedLocations", digits, spat_length_covered, "\n",
+  cat("fixedLocations", digits, spat_length_covered_95,
+      spat_length_covered_80, spat_length_covered_50, "\n",
       file = "output/simulation.txt", append = TRUE, sep=" ")
   
 }
